bb13a853cf23e864728a225a45f9d01955718957
CA <contractaddress@proton.me>
2024-09-07 09:18:19 +0200

    :>

--- modular_arithemtic/temp.py
+++ modular_arithemtic/temp.py
@@ -5,4 +5,7 @@ ints = [5,14,6,11,58, 32]
 for intgr in ints:
     resulz = pow(intgr, (p - 1)//2, p)
     if resulz == 1:
-        print(intgr)
+     print(intgr)
+
+
+# testing legendre with ints and p
