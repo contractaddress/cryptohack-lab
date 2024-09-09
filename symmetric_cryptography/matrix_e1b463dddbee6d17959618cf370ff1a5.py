2443b9b6196cf9691ee664315a5e448baa6f04d1
CA <contractaddress@proton.me>
2024-09-09 19:51:44 +0200

    starting symm crypt

---
+++ symmetric_cryptography/matrix_e1b463dddbee6d17959618cf370ff1a5.py
@@ -0,0 +1,28 @@
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """ #my solution below
+    stringlist = []
+    for row in matrix:
+      for item in row:
+        stringlist.append(chr(item))
+    
+    return ''.join(stringlist)
+
+    
+ 
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(matrix))
+
+
+
+#output: crypto{inmatrix}
+
